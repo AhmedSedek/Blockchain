3a2db3011c2f3bfb72267ebe2dd32876aeedfab8
MuhammadSharaf <m.sharf47@gmail.com>
2020-05-19 01:45:57 +0200

    add client ports

--- Connect.py
+++ Connect.py
@@ -5,8 +5,8 @@ from threading import Lock
 
 s_print_lock = Lock()
 HOST = "127.0.0.1"
-MINERS_PORT_LIST = [65433, 65434, 65435]
-CLIENTS_PORT_LIST = []
+CLIENTS_PORT_LIST = [65430, 65431, 65432, 65433]
+MINERS_PORT_LIST = [65434, 65435, 65436, 65437]
 
 def t_print(*a, **b):
     with s_print_lock:
@@ -14,26 +14,43 @@ def t_print(*a, **b):
 
 class Connect:
 
-    def __init__(self, port, callbacks):
+    def __init__(self, port, callbacks = {}):
         self.PORT = port
         self.callbacks = callbacks
 
         _thread.start_new_thread(self._receiver, ())
 
     def send_to_all_miners(self, data_type, data):
-        connect_data = ConnectData(data_type, data)
-        data = pickle.dumps(connect_data)
-        for port in MINERS_PORT_LIST:
-            if port != self.PORT:
-                t_print("sending to", port)
-                self._send(port, data)
+        self._send_to_all_ports(MINERS_PORT_LIST, "Miner", data_type, data)
+
+    def send_to_all_clients(self, data_type, data):
+        self._send_to_all_ports(CLIENTS_PORT_LIST, "Client", data_type, data)
 
     def send_to_miner(self, port, data_type, data):
+        self._send_to_port("Miner", port, data_type, data)
+
+    def send_to_client(self, port, data_type, data):
+        self._send_to_port("Client", port, data_type, data)
+
+    def _send_to_port(self, kind, port, data_type, data):
         connect_data = ConnectData(data_type, data)
         data = pickle.dumps(connect_data)
-        t_print("sending to", port)
-        self._send(port, data)
+        t_print("sending to", kind, port)
+        try:
+            self._send(port, data)
+        except (ValueError, Exception):
+            t_print(kind, port, "Not Connected")
 
+    def _send_to_all_ports(self, port_list, kind,  data_type, data):
+        connect_data = ConnectData(data_type, data)
+        data = pickle.dumps(connect_data)
+        for port in port_list:
+            if port != self.PORT:
+                t_print("sending to", port)
+                try:
+                    self._send(port, data)
+                except (ValueError, Exception):
+                    t_print(kind,port, "Not Connected")
 
     def _send(self, port, data):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -59,5 +76,9 @@ class Connect:
                     connect_data = pickle.loads(bytes(data))
                     data_type = connect_data.TYPE
                     data = connect_data.get_data()
-                    self.callbacks[data_type](data)
+                    try:
+                        self.callbacks[data_type](data)
+                    except (ValueError, Exception):
+                        t_print("No Supported Callback")
+
 
--- testNetwork.py
+++ testNetwork.py
@@ -11,15 +11,23 @@ def miner2(data):
 def miner3(data):
     print("I'm miner 3 received ", data)
 
+def miner2_second_callback(data):
+    print("I'm miner 2 received Second callback", data)
+
 miner1 = Connect(65433, {ConnectData.TYPE_TRANSACTION: miner1})
-miner2 = Connect(65434, {ConnectData.TYPE_TRANSACTION: miner2})
-miner3 = Connect(65435, {ConnectData.TYPE_TRANSACTION: miner3})
+miner2 = Connect(65434, {ConnectData.TYPE_TRANSACTION: miner2, ConnectData.TYPE_BLOCK: miner2_second_callback})
+miner3 = Connect(65435)
+
+# client1 = Connect(65430, {ConnectData.TYPE_TRANSACTION: miner3})
+# client2= Connect(65431, {ConnectData.TYPE_TRANSACTION: miner3})
+# client3 = Connect(65432, {ConnectData.TYPE_TRANSACTION: miner3})
+# client4 = Connect(65433, {ConnectData.TYPE_TRANSACTION: miner3})
 
 while True:
-    m = input("Transaction Msg " )
-    miner1.send_to_all_miners(ConnectData.TYPE_TRANSACTION, m)
+    # m = input("Test Msg " )
+    # miner1.send_to_all_miners(ConnectData.TYPE_TRANSACTION, m)
 
-    m = input("Transaction Msg ")
-    miner2.send_to_all_miners(ConnectData.TYPE_TRANSACTION, m)
+    m = input("Test Msg ")
+    miner3.send_to_all_miners(ConnectData.TYPE_BLOCK, m)
 
 
